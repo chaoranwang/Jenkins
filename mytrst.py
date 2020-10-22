f47dc902ec9a663d8709b86420d9a496f5b32074
chaoranwang <270790167@qq.com>
2020-10-22 10:02:01 +0800

    Update a new  file~

--- mytrst.py
+++ mytrst.py
@@ -3,3 +3,23 @@
 # @Author  : Chaorn Wang
 # @Email   : chaoran.wcr@dtwave-inc.com
 # @File    : mytrst.py
+
+
+def numb(s):
+ result={}
+ for i in s:
+    if i in result.keys():
+        result[i]=result[i]+1
+    else:
+        result[i]=1
+ return result
+fu = "aaaannnnnnffieeeefasdnnnn"
+f = numb(fu)
+n = list(f.values())
+n.sort()
+print(n)
+print(n[-2])
+i = n[-2]
+for item in f.keys():
+  if i == f[item]:
+    print(item)
